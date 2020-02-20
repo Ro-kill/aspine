0e3ed2d4d992e1b7ba8a60ad3af0132c754017cb
kdk1616 <31357347+kdk1616@users.noreply.github.com>
2020-02-20 14:28:11 -0500

    Added Firebase integration (package.json). Added firechat code to home.html.(Had to setup database on my firebase account

---
+++ public/test.js
@@ -0,0 +1,19 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase");
+
+
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+   apiKey: "AIzaSyBdmLyMLvm76_v28O13AFg3m5xrSV9uciw",
+   authDomain: "aspine-2bae6.firebaseapp.com",
+   databaseURL: "https://aspine-2bae6.firebaseio.com",
+   projectId: "aspine-2bae6",
+   storageBucket: "aspine-2bae6.appspot.com",
+   messagingSenderId: "228269869217",
+   appId: "1:228269869217:web:1bfb40a72ab1b0d16088e0",
+   measurementId: "G-CK8CM2GP56"
+ };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
